ad9f213dc5874d61feaffad13d349e8d718ad0b0
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-27 10:55:25 +0800

    updated props to take setAccounts function and removed onSave prop. can now send patch request to update account details

--- src/components/Accounts/EditAccount.js
+++ src/components/Accounts/EditAccount.js
@@ -8,15 +8,32 @@ import {
 } from '@mui/material';
 import AccountDetails from './AccountDetails';
 
-const EditAccount = ({ open, onClose, account, onSave }) => {
-  const [editedAccount, setEditedAccount] = useState(account);
+const EditAccount = ({ open, onClose, account, setAccounts }) => {
+  const [editAccount, setEditAccount] = useState(account);
 
   const handleFieldChange = (field, value) => {
-    setEditedAccount({ ...editedAccount, [field]: value });
+    setEditAccount({ ...editAccount, [field]: value });
   };
 
-  const handleSave = () => {
-    onSave(editedAccount);
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/v1/api/update_account", {
+      method: "PATCH", body: JSON.stringify(editAccount),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setAccounts(accounts => {
+        const updatedAccountIndex = accounts.findIndex(account => account.id === editAccount.id);
+        if (updatedAccountIndex !== -1) {
+          accounts[updatedAccountIndex] = editAccount;
+        }
+        return [...accounts]
+      });
+      console.log(data);
+    }
     onClose();
   };
 
@@ -24,7 +41,7 @@ const EditAccount = ({ open, onClose, account, onSave }) => {
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Edit Account</DialogTitle>
       <DialogContent>
-        <AccountDetails account={editedAccount} onChange={handleFieldChange} />
+        <AccountDetails account={editAccount} onChange={handleFieldChange} />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
