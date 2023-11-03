9ae19a471296237259b2bc543d590cdbeffbc009
Daniel Bautista <eindal.bautista@gmail.com>
2023-11-03 17:33:53 +0800

    created onRowUpdate function to send patch requests, the request body will only contain changed values instead of whole row. added editable attributes for columns and implemented valueGetter and valueFormatter for date type columns. implemented AlertSnackbar to display messages if update is successful or failed. removed EditAccount and AccountDetails components in favor of DataGrid update function

--- src/components/Accounts/AccountsDataGrid.jsx
+++ src/components/Accounts/AccountsDataGrid.jsx
@@ -2,38 +2,50 @@ import React, { useEffect, useState } from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { SERVER } from '../../App';
 import { useOutletContext } from 'react-router-dom';
+import { getObjectNewValues } from '../../utils/getObjectNewValues';
+import AlertSnackbar from '../Alerts/AlertSnackbar';
 const moment = require('moment');
 
-const categories = ['Market', 'Finance', 'Development']
+const categories = ['Test 1', 'Test 2', 'Test 3']
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'username', headerName: 'Username', width: 100 },
-    { field: 'password', headerName: 'Password', width: 100 },
+    { field: 'password', headerName: 'Password', width: 100, editable: true },
     { field: 'level', headerName: 'Level', width: 80 },
     {
         field: 'category',
-        headerName: 'Category', width: 100,
+        headerName: 'Category', width: 100, editable: true,
         type: 'singleSelect',
         valueOptions: categories
     },
-    { field: 'date_registered', headerName: 'Registration', width: 100, type: 'date' },
-    { field: 'verified', headerName: 'Verified', width: 80 },
-    { field: 'last_login', headerName: 'Last Login', width: 100 },
-    { field: 'account_url', headerName: 'Profile URL', width: 120 },
-    { field: 'recovery_email', headerName: 'Recovery Email', width: 120 },
-    { field: 'name', headerName: 'Name', width: 120 },
+    {
+        field: 'date_registered',
+        headerName: 'Registration', width: 100, type: 'date',
+        editable: true,
+        valueGetter: (params) => {
+            return params.value ? moment(params.value, 'YYYY-MM-DD').toDate() : '';
+        },
+        valueFormatter: (params) => {
+            return params.value ? moment(params.value).format('DD/MM/YYYY') : '';
+        }
+    },
+    { field: 'verified', headerName: 'Verified', width: 80, editable: true },
+    { field: 'last_login', headerName: 'Last Login', width: 100, editable: true },
+    { field: 'account_url', headerName: 'Profile URL', width: 120, editable: true },
+    { field: 'recovery_email', headerName: 'Recovery Email', width: 120, editable: true },
+    { field: 'name', headerName: 'Name', width: 120, editable: true },
     {
         field: 'date_of_birth',
         headerName: 'Date of Birth', width: 100, type: 'date',
         editable: true,
         valueGetter: (params) => {
-            return moment(params.value, 'YYYY-MM-DD').toDate();
+            return params.value ? moment(params.value, 'YYYY-MM-DD').toDate() : '';
         },
         valueFormatter: (params) => {
-            return moment(params.value).format('DD/MM/YYYY');
+            return params.value ? moment(params.value).format('DD/MM/YYYY') : '';
         }
     },
-    { field: 'mobile_no', headerName: 'Mobile No', width: 100 },
+    { field: 'mobile_no', headerName: 'Mobile No', width: 100, editable: true },
     {
         field: 'gender',
         headerName: 'Gender', width: 100,
@@ -45,6 +57,11 @@ const columns = [
 
 const AccountsDataGrid = () => {
     const [accounts, setAccounts] = useState([]);
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        severity: "",
+        description: ""
+    });
     const [token] = useOutletContext();
 
     useEffect(() => {
@@ -66,37 +83,66 @@ const AccountsDataGrid = () => {
     }, []);
 
     const onRowUpdate = async (updatedRow, originalRow) => {
-        return updatedRow;
+        const newValues = getObjectNewValues(originalRow, updatedRow);
+        if (Object.keys(newValues).length === 0) {
+            return originalRow;
+        }
+        newValues['id'] = updatedRow['id'];
+
+        const response = await fetch(SERVER + "/update_account", {
+            method: "PATCH", body: JSON.stringify(newValues),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token,
+            }
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setSnackbar({ open: true, severity: 'success', description: data })
+            return updatedRow;
+        } else {
+            setSnackbar({ open: true, severity: 'error', description: data })
+            return originalRow;
+        }
     };
 
     return (
-        <DataGrid
-            sx={{
-                '.MuiDataGrid-columnHeaders': {
-                    backgroundColor: '#e7e5e1',
-                },
-                maxHeight: '80vh',
-                maxWidth: '75vw',
-            }}
-            rows={accounts}
-            columns={columns}
-            slots={{ toolbar: GridToolbar }}
-            slotProps={{
-                toolbar: {
-                    showQuickFilter: true,
-                },
-            }}
-            initialState={{
-                pagination: {
-                    paginationModel: { page: 0, pageSize: 10 },
-                },
-            }}
-            pageSizeOptions={[5, 10, 20]}
-            checkboxSelection
-            disableRowSelectionOnClick
-            processRowUpdate={onRowUpdate}
-            onProcessRowUpdateError={(error) => console.log(error)}
-        />
+        <>
+            <DataGrid
+                sx={{
+                    '.MuiDataGrid-columnHeaders': {
+                        backgroundColor: '#e7e5e1',
+                    },
+                    maxHeight: '80vh',
+                    maxWidth: '75vw',
+                }}
+                rows={accounts}
+                columns={columns}
+                slots={{ toolbar: GridToolbar }}
+                slotProps={{
+                    toolbar: {
+                        showQuickFilter: true,
+                    },
+                }}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 10 },
+                    },
+                }}
+                pageSizeOptions={[5, 10, 20]}
+                checkboxSelection
+                disableRowSelectionOnClick
+                processRowUpdate={onRowUpdate}
+                onProcessRowUpdateError={(error) => console.log(error)}
+            />
+            <AlertSnackbar
+                open={snackbar.open}
+                severity={snackbar.severity}
+                description={snackbar.description}
+                onClose={() => setSnackbar(snackbar => ({ ...snackbar, open: false }))}
+            />
+        </>
     );
 }
 
