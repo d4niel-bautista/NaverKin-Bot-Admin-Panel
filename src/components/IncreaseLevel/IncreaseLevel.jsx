a7682771b367ea6422ab4fc160c82b117add543a
Daniel Bautista <eindal.bautista@gmail.com>
2023-11-10 16:45:19 +0800

    updated to use BotConfigs and PromptConfigs components. added section for a dropdown to select an account to increase level. added function to send request to the server to start the autoanswer bot

--- src/components/IncreaseLevel/IncreaseLevel.jsx
+++ src/components/IncreaseLevel/IncreaseLevel.jsx
@@ -1,35 +1,9 @@
-import { Box, Button, Divider, Grid, Menu, MenuItem, TextField, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Backdrop, Box, Button, CircularProgress, Divider, Grid, Menu, MenuItem, TextField, Typography } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
 import { SERVER } from '../../App';
 import { useOutletContext } from 'react-router-dom';
-
-const delayValues = {
-    '30 secs': 30,
-    '1 min': 60,
-    '2 mins': 120,
-    '3 mins': 180,
-    '5 mins': 300,
-    '10 mins': 600,
-    '15 mins': 900,
-    '20 mins': 1200,
-    '30 mins': 1800,
-    '45 mins': 2700,
-    '1 hour': 3600,
-    '2 hours': 7200,
-    '3 hours': 10800
-};
-
-const cooldownValues = {
-    '16 hours': 57600,
-    '18 hours': 64800,
-    '20 hours': 72000,
-    '24 hours': 86400
-}
-
-const answersPerDay = [];
-for (let i = 1; i < 21; i++) {
-    answersPerDay.push(i * 10);
-}
+import BotConfigs from './BotConfigs';
+import PromptConfigs from './PromptConfigs';
 
 const IncreaseLevel = () => {
     const [botConfigs, setBotConfigs] = useState({ 'submit_delay': 120, 'page_refresh': 600, 'answers_per_day': 20, 'cooldown': 64800 });
@@ -37,6 +11,8 @@ const IncreaseLevel = () => {
     const [promptConfigsList, setPromptConfigsList] = useState([]);
     const [promptConfigs, setPromptConfigs] = useState({ 'id': '', 'prompt': '', 'postscript': '', 'prohibited_words': '' });
     const [levelupAccounts, setLevelupAccounts] = useState([]);
+    const [levelupAccount, setLevelupAccount] = useState();
+    const [loadingState, setLoadingState] = useState(false);
     const [token] = useOutletContext();
 
     useEffect(() => {
@@ -54,23 +30,16 @@ const IncreaseLevel = () => {
                 setBotConfigs(data.botconfigs);
                 setTempBotConfigs(data.botconfigs);
                 setPromptConfigsList(data.prompt_configs);
+                setPromptConfigs(data.prompt_configs[0]);
                 setLevelupAccounts(data.levelup_accounts);
             }
         }
         fetchAutoanswerBotConfigs();
     }, []);
 
-    const changePromptConfigs = async (e) => {
-        const { name, value } = e.target;
-        setPromptConfigs({
-            ...promptConfigs,
-            [name]: value,
-        });
-    };
-
-    const changeBotConfigs = async (e) => {
-        const { name, value } = e.target;
-        setTempBotConfigs({ ...tempBotConfigs, [name]: value });
+    const changeLevelupAccount = async (e) => {
+        const { value } = e.target;
+        setLevelupAccount(levelupAccounts.find(account => account.id === value));
     };
 
     const revertChanges = () => {
@@ -102,86 +71,76 @@ const IncreaseLevel = () => {
         }
     };
 
-    return (
-        <Box sx={{ border: 1, borderColor: '#e0e0e0', borderRadius: 1, padding: '12px' }}>
-            <Typography variant='h5' marginBottom={3}>
-                Configuration
-            </Typography>
-            <Grid container rowSpacing={3}>
-                <Grid item md={12}>
-                    <TextField select label="Submit Delay" name="submit_delay" onChange={changeBotConfigs} value={tempBotConfigs['submit_delay']} sx={{ width: '150px', marginRight: 2 }}>
-                        {Object.keys(delayValues).map((key) =>
-                            <MenuItem key={key} value={delayValues[key]}>{key}</MenuItem>
-                        )}
-                    </TextField>
-                    <TextField select label="Next Question" name="page_refresh" onChange={changeBotConfigs} value={tempBotConfigs['page_refresh']} defaultValue={20} sx={{ width: '150px', marginRight: 2 }}>
-                        {Object.keys(delayValues).map((key) =>
-                            <MenuItem key={key} value={delayValues[key]}>{key}</MenuItem>
-                        )}
-                    </TextField>
-                    <TextField select label="Answers per Day" name="answers_per_day" onChange={changeBotConfigs} value={tempBotConfigs['answers_per_day']} sx={{ width: '170px', marginRight: 2 }}>
-                        {answersPerDay.map((num) =>
-                            <MenuItem key={num} value={num}>{num}</MenuItem>
-                        )}
-                    </TextField>
-                    <TextField select label="Restart After" name="cooldown" onChange={changeBotConfigs} value={tempBotConfigs['cooldown']} sx={{ width: '170px', marginRight: 2 }}>
-                        {Object.keys(cooldownValues).map((key) =>
-                            <MenuItem key={key} value={cooldownValues[key]}>{key}</MenuItem>
-                        )}
-                    </TextField>
-                </Grid>
-                <Grid item md={12}>
-                    <TextField select label="Category" name="description" defaultValue={''} sx={{ width: '160px', marginRight: 2 }} onChange={(e) => setPromptConfigs({ 'id': e.target.value['id'], 'prompt': e.target.value['prompt'], 'postscript': e.target.value['postscript'], 'prohibited_words': e.target.value['prohibited_words'] })}>
-                        {promptConfigsList.map((item) =>
-                            <MenuItem key={item.id} value={item}>{item.description}</MenuItem>
-                        )}
-                    </TextField>
-                    <TextField
-                        name='prompt'
-                        label='Prompt'
-                        sx={{ width: '300px', marginRight: 2 }}
-                        margin="dense"
-                        rows={8}
-                        value={promptConfigs.prompt}
-                        multiline
-                        onChange={changePromptConfigs}
-                    />
-                    <TextField
-                        name='postscript'
-                        label='Postscript'
-                        sx={{ width: '200px', marginRight: 2 }}
-                        margin="dense"
-                        rows={8}
-                        value={promptConfigs.postscript}
-                        multiline
-                        onChange={changePromptConfigs}
-                    />
-                    <TextField
-                        name='prohibited_words'
-                        label='Prohibited Words'
-                        sx={{ width: '200px' }}
-                        margin="dense"
-                        rows={8}
-                        value={promptConfigs.prohibited_words}
-                        multiline
-                        onChange={changePromptConfigs}
-                    />
-                </Grid>
-                <Grid container item columnSpacing={2} sx={{ marginTop: -2, marginLeft: 21 }}>
-                    <Grid item>
-                        <Button color="secondary" variant="contained" onClick={revertChanges}>
-                            Cancel
-                        </Button>
-                    </Grid>
-                    <Grid item>
-                        <Button color="primary" variant="contained" onClick={updateChanges}>
-                            Update
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Grid>
+    const startAutoanswerBot = async (e) => {
+        e.preventDefault();
+        setLoadingState(true);
 
-        </Box>
+        if (!levelupAccount) {
+            return;
+        }
+
+        const autoanswerBotData = { 'levelup_account': levelupAccount, 'botconfigs': botConfigs, 'prompt_configs': promptConfigs }
+        const response = await fetch(SERVER + "/start_autoanswerbot", {
+            method: "POST",
+            body: JSON.stringify(autoanswerBotData),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token,
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+        }
+        setLoadingState(false);
+    };
+
+    return (
+        <>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loadingState}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <Box sx={{ border: 1, borderColor: '#e0e0e0', borderRadius: 1, padding: '12px', marginBottom: 2 }}>
+                <Typography variant='h5' marginBottom={3}>
+                    Configuration
+                </Typography>
+                <Grid container rowSpacing={3}>
+                    <BotConfigs tempBotConfigs={tempBotConfigs} setTempBotConfigs={setTempBotConfigs} />
+                    <PromptConfigs promptConfigs={promptConfigs} setPromptConfigs={setPromptConfigs} />
+                    <Grid container item columnSpacing={2} sx={{ marginTop: -2, marginLeft: 21 }}>
+                        <Grid item>
+                            <Button color="secondary" variant="contained" onClick={revertChanges} disabled={loadingState}>
+                                Cancel
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button color="primary" variant="contained" onClick={updateChanges} disabled={loadingState}>
+                                Update
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box sx={{ border: 1, borderColor: '#e0e0e0', borderRadius: 1, padding: '12px' }}>
+                <Grid container columnSpacing={2} alignItems={'center'}>
+                    <Grid item>
+                        <TextField select label="Account" name="levelup_account" defaultValue='' onChange={changeLevelupAccount} sx={{ width: '200px' }}>
+                            {levelupAccounts.map((account) =>
+                                <MenuItem key={account.id} value={account.id}>{account.username}</MenuItem>
+                            )}
+                        </TextField>
+                    </Grid>
+                    <Grid item>
+                        <Button color="primary" variant="contained" onClick={startAutoanswerBot} disabled={!levelupAccount || loadingState}>
+                            Start
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
     );
 };
 
