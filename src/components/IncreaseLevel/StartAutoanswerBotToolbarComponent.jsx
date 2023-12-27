6c209400ffc4b68864b31d75ed43d773934afaa7
Daniel Bautista <eindal.bautista@gmail.com>
2023-12-27 16:35:26 +0800

    added StartAutoanswerBotToolbarComponent to handle searching accounts and triggering start function

---
+++ src/components/IncreaseLevel/StartAutoanswerBotToolbarComponent.jsx
@@ -0,0 +1,30 @@
+import { Button, Grid } from '@mui/material';
+import { GridToolbarContainer, GridToolbarQuickFilter } from '@mui/x-data-grid';
+import React from 'react';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+
+const StartAutoanswerBotToolbarComponent = ({ startAutoanswerBot, rowSelectionModel, loadingState }) => {
+    return (
+        <GridToolbarContainer>
+            <Grid container columnSpacing={12} rowSpacing={2}>
+                <Grid item xs={6} sm={6} md={6} xl={6} >
+                    <Button
+                        color="primary"
+                        size='small'
+                        startIcon={<KeyboardDoubleArrowUpIcon />}
+                        sx={{ fontWeight: 'bold' }}
+                        onClick={startAutoanswerBot}
+                        disabled={!rowSelectionModel.length || loadingState}
+                    >
+                        Start
+                    </Button>
+                </Grid>
+                <Grid item xs={6} sm={6} md={6} xl={6} container justifyContent="flex-end">
+                    <GridToolbarQuickFilter />
+                </Grid>
+            </Grid>
+        </GridToolbarContainer>
+    );
+};
+
+export default StartAutoanswerBotToolbarComponent;
