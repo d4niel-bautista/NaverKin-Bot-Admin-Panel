3197a23e560c9683f469b5d34077642818376dd2
Daniel Bautista <eindal.bautista@gmail.com>
2024-01-04 17:10:30 +0800

    added fetchAccounts() upon rendering QuestionAnswerForm and implemented combineObjectArrayByProperty() to merge the fetched list of accounts into one object

--- src/components/QuestionAnswerForm/QuestionAnswerForm.jsx
+++ src/components/QuestionAnswerForm/QuestionAnswerForm.jsx
@@ -7,6 +7,7 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import { AuthContext } from '../../context/AuthProvider';
 import { ServerAPIContext } from '../../context/ServerAPIProvider';
 import AccountsSelection from './AccountsSelection';
+import { combineObjectArrayByProperty } from '../../utils/combineObjectArrayByProperty';
 
 const QuestionAnswerForm = () => {
     const [formType, setFormType] = useState("1:2")
@@ -21,6 +22,7 @@ const QuestionAnswerForm = () => {
     const [answerExposurePromptConfigs, setAnswerExposurePromptConfigs] = useState({ 'query': '', 'prompt': '' });
     const [prohibitedWords, setProhibitedWords] = useState("");
     const [interactions, setInteractions] = useState([]);
+    const [accounts, setAccounts] = useState({});
     const [token] = useContext(AuthContext);
     const [serverAPI] = useContext(ServerAPIContext);
 
@@ -41,7 +43,7 @@ const QuestionAnswerForm = () => {
                 setAnswerExposurePromptConfigs({ 'query': data['answer_exposure']['query'], 'prompt': data['answer_exposure']['prompt'] });
                 setProhibitedWords(data['prohibited_words']);
             }
-        }
+        };
         const fetchInteractions = async () => {
             const response = await fetch(serverAPI + "/interactions", {
                 method: "GET",
@@ -56,8 +58,25 @@ const QuestionAnswerForm = () => {
                 setInteractions(data);
             }
         };
+        const fetchAccounts = async () => {
+            const response = await fetch(serverAPI + "/accounts", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token,
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const combinedObject = combineObjectArrayByProperty(data, 'username');
+                setAccounts(combinedObject);
+                console.log(combinedObject);
+            }
+        };
         fetchInteractions();
         getPromptConfigs();
+        fetchAccounts();
     }, [])
 
     const handleTextChange = (field, text) => {
